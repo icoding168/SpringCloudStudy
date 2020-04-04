89c68f8f505f1366bc07aad557be4d18f7f83ae2
icoding168 <icoding168@qq.com>
2020-04-05 03:52:18 +0800

    Ribbon 客户端负载均衡

--- eureka-client-2/src/main/java/xyz/icoding168/scs/eureka/EurekaClient2.java
+++ eureka-client-2/src/main/java/xyz/icoding168/scs/eureka/EurekaClient2.java
@@ -3,10 +3,11 @@ package xyz.icoding168.scs.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-
+@RibbonClient(name = "ribbonClient", configuration = RibbonConfiguration.class)
 public class EurekaClient2 {
 
     public static void main(String[] args){
--- eureka-client-3/src/main/java/xyz/icoding168/scs/eureka/EurekaClient3.java
+++ eureka-client-3/src/main/java/xyz/icoding168/scs/eureka/EurekaClient3.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaClient1 {
+public class EurekaClient3 {
 
     public static void main(String[] args){
-        SpringApplication.run(EurekaClient1.class, args);
+        SpringApplication.run(EurekaClient3.class, args);
     }
 }
